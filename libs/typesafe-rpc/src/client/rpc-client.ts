ae8a8d289c7857aeb79e34aa8ce66627b5a0419d
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2026-02-04 08:15:39 +0000

    feat: enhance createRpcClient to support context parameter for dynamic headers

--- libs/typesafe-rpc/src/client/rpc-client.ts
+++ libs/typesafe-rpc/src/client/rpc-client.ts
@@ -1,4 +1,4 @@
-import type { RpcSchema } from '../shared';
+import type { BaseContext, RpcSchema } from '../shared';
 import { fetchData } from './fetch-data';
 
 type RpcClient<T extends RpcSchema> = {
@@ -6,13 +6,14 @@ type RpcClient<T extends RpcSchema> = {
     [L in keyof T[K]]: (
       params: Parameters<T[K][L]>[0]['params'],
       signal?: AbortSignal,
+      context?: Parameters<T[K][L]>[0]['context'],
     ) => ReturnType<T[K][L]>;
   };
 };
 
-export function createRpcClient<T extends RpcSchema>(
+export function createRpcClient<T extends RpcSchema, Context extends BaseContext>(
   endpoint: string,
-  headers?: HeadersInit,
+  headers?: HeadersInit | ((context: Context) => HeadersInit),
 ): RpcClient<T> {
   return new Proxy(
     {},
@@ -21,10 +22,10 @@ export function createRpcClient<T extends RpcSchema>(
         new Proxy(
           {},
           {
-            get: (_, operation: string) => (params: any, signal?: AbortSignal) =>
+            get: (_, operation: string) => (params: any, signal?: AbortSignal, context?: any) =>
               fetchData(`${endpoint}?${entity}::${operation}`, 'POST', {
                 body: JSON.stringify({ entity, operation, params }),
-                headers,
+                headers: typeof headers === 'function' ? headers(context) : headers,
                 signal,
               }),
           },
